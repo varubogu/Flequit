d493992e93326c61bc338ae628d732c4a8870cee
varubogu <toyosuke@varubogu.com>
2025-01-06 10:12:31 +0900

    不要なものを削除

--- src/types/state/url-state.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const urlStateSchema = z.object({
-  daily: z.string().nullable(),
-  project: z.string().nullable(),
-  taskList: z.string().nullable(),
-});
-
-export type UrlState = z.infer<typeof urlStateSchema>;
